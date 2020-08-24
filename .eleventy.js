531e437514b37569645c2d85109d5aa5266cb481
Grimbsi <quentin.madura@gmail.com>
2020-08-24 17:39:57 +0200

    Add addPassthroughCopy for CSS files

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  // it allows to copy the css and js in the _site folder
   // eleventyConfig.addPassthroughCopy("js");
+  // eleventyConfig.addPassthroughCopy('css')
 
   // You can return your Config object (optional).
   return {
+    // passthroughFileCopy: true,
     dir: {
       input: ".",
       output: "_site"
