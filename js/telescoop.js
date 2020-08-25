70fa16b7c0195475a332de92538633ebb64f738c
Grimbsi <quentin.madura@gmail.com>
2020-08-25 20:56:53 +0200

    [MAJOR] Change main structure, update identity of the page, initialise global data files configuration

--- js/telescoop.js
+++ js/telescoop.js
@@ -5,14 +5,14 @@ var employees = document.getElementsByClassName("employee");
 
 // nice scroll to anchors
 // https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
-document.querySelectorAll('a[href^="#"]').forEach(function(anchor){
-    anchor.addEventListener('click', function (e) {
-    e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-  });
 });
 
 // --------------------------------------------
@@ -24,55 +24,57 @@ rights[rights.length - 1].style.display = 'none';
 // hide all clients but fort one
 console.log('hide clients', clients);
 for (var i = 1; i < clients.length; i++) {
-  clients[i].classList.add('hide');
+    clients[i].classList.add('hide');
 }
 
 function nextClient() {
-  console.log('### next client', currentClient);
-  currentClient += 1;
-  if (currentClient >= clients.length) {
-    currentClient = clients.length - 1;
-  }
-  updateShowItems(clients, currentClient, false);
+    console.log('### next client', currentClient);
+    currentClient += 1;
+    if (currentClient >= clients.length) {
+        currentClient = clients.length - 1;
+    }
+    updateShowItems(clients, currentClient, false);
 }
+
 function previousClient() {
-  console.log('### prev client', currentClient);
-  currentClient -= 1;
-  if (currentClient < 0) {
-    currentClient = 0;
-  }
-  updateShowItems(clients, currentClient, false);
+    console.log('### prev client', currentClient);
+    currentClient -= 1;
+    if (currentClient < 0) {
+        currentClient = 0;
+    }
+    updateShowItems(clients, currentClient, false);
 }
 
 function nextEmployee() {
-  console.log('### next client', currentEmployee);
-  currentEmployee += 1;
-  if (currentEmployee >= employees.length) {
-    currentEmployee = employees.length - 1;
-  }
-  updateShowItems(employees, currentEmployee, false);
+    console.log('### next client', currentEmployee);
+    currentEmployee += 1;
+    if (currentEmployee >= employees.length) {
+        currentEmployee = employees.length - 1;
+    }
+    updateShowItems(employees, currentEmployee, false);
 }
+
 function previousEmployee() {
-  console.log('### prev client', currentEmployee);
-  currentEmployee -= 1;
-  if (currentEmployee < 0) {
-    currentEmployee = 0;
-  }
-  updateShowItems(employees, currentEmployee, false);
+    console.log('### prev client', currentEmployee);
+    currentEmployee -= 1;
+    if (currentEmployee < 0) {
+        currentEmployee = 0;
+    }
+    updateShowItems(employees, currentEmployee, false);
 }
 
 function updateShowItems(items, currentItem, reverse) {
-  for (var ix = 0; ix < items.length; ix++) {
-    var itemIx = reverse ? items.length - 1 - ix : ix;
-    var item = items[itemIx];
-    if (itemIx === currentItem) {
-      item.classList.add("show");
-      item.classList.remove("hide");
-      console.log('show', item, reverse);
-    } else {
-      console.log('hide', item, reverse);
-      item.classList.add("hide");
-      item.classList.remove("show");
+    for (var ix = 0; ix < items.length; ix++) {
+        var itemIx = reverse ? items.length - 1 - ix : ix;
+        var item = items[itemIx];
+        if (itemIx === currentItem) {
+            item.classList.add("show");
+            item.classList.remove("hide");
+            console.log('show', item, reverse);
+        } else {
+            console.log('hide', item, reverse);
+            item.classList.add("hide");
+            item.classList.remove("show");
+        }
     }
-  }
 }
