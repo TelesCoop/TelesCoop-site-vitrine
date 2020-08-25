78d0524c943bd8d3a048766e417bae3131a812aa
Grimbsi <quentin.madura@gmail.com>
2020-08-25 20:52:09 +0200

    Update eleventy configuration and configure a plugin to handle svg in HTML files templates

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,17 @@
+const svgContents = require('eleventy-plugin-svg-contents');
+
 module.exports = function (eleventyConfig) {
-  // it allows to copy the css and js in the _site folder
-  // eleventyConfig.addPassthroughCopy("js");
-  // eleventyConfig.addPassthroughCopy('css')
+  // Copy the static assets to the output folder
+  eleventyConfig.addPassthroughCopy('js');
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPlugin(svgContents);
 
   // You can return your Config object (optional).
   return {
-    // passthroughFileCopy: true,
+    passthroughFileCopy: true,
     dir: {
-      input: ".",
-      output: "_site"
-    }
+      input: '.',
+      output: '_site',
+    },
   };
 };
