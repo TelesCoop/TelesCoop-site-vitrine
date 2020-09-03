a6496e4816ab7886740cf5085acf77a26cbd3b4d
Grimbsi <quentin.madura@gmail.com>
2020-09-03 16:20:41 +0200

    website improvements

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,17 @@
 const svgContents = require('eleventy-plugin-svg-contents');
 
-module.exports = function (eleventyConfig) {
-  // Copy the static assets to the output folder
-  eleventyConfig.addPassthroughCopy('js');
-  eleventyConfig.addPassthroughCopy('css');
-  eleventyConfig.addPlugin(svgContents);
+module.exports = function(eleventyConfig) {
+    // Copy the static assets to the output folder
+    eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPlugin(svgContents);
 
-  // You can return your Config object (optional).
-  return {
-    passthroughFileCopy: true,
-    dir: {
-      input: '.',
-      output: '_site',
-    },
-  };
+    // You can return your Config object (optional).
+    return {
+        passthroughFileCopy: true,
+        dir: {
+            input: '.',
+            output: '_site',
+        },
+    };
 };
--- js/telescoop.js
+++ js/telescoop.js
@@ -1,8 +1,3 @@
-var currentClient = 0;
-var currentEmployee = 0;
-var clients = document.getElementsByClassName("client");
-var employees = document.getElementsByClassName("employee");
-
 // nice scroll to anchors
 // https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
 document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
@@ -10,71 +5,9 @@ document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+            behavior: 'smooth',
         });
     });
 });
 
-// --------------------------------------------
-// CLIENT
-// hide unused arrows
-document.querySelector(".client .fa-chevron-left").style.display = 'none';
-var rights = document.querySelectorAll(".client .fa-chevron-right");
-rights[rights.length - 1].style.display = 'none';
-// hide all clients but fort one
-console.log('hide clients', clients);
-for (var i = 1; i < clients.length; i++) {
-    clients[i].classList.add('hide');
-}
-
-function nextClient() {
-    console.log('### next client', currentClient);
-    currentClient += 1;
-    if (currentClient >= clients.length) {
-        currentClient = clients.length - 1;
-    }
-    updateShowItems(clients, currentClient, false);
-}
-
-function previousClient() {
-    console.log('### prev client', currentClient);
-    currentClient -= 1;
-    if (currentClient < 0) {
-        currentClient = 0;
-    }
-    updateShowItems(clients, currentClient, false);
-}
-
-function nextEmployee() {
-    console.log('### next client', currentEmployee);
-    currentEmployee += 1;
-    if (currentEmployee >= employees.length) {
-        currentEmployee = employees.length - 1;
-    }
-    updateShowItems(employees, currentEmployee, false);
-}
-
-function previousEmployee() {
-    console.log('### prev client', currentEmployee);
-    currentEmployee -= 1;
-    if (currentEmployee < 0) {
-        currentEmployee = 0;
-    }
-    updateShowItems(employees, currentEmployee, false);
-}
-
-function updateShowItems(items, currentItem, reverse) {
-    for (var ix = 0; ix < items.length; ix++) {
-        var itemIx = reverse ? items.length - 1 - ix : ix;
-        var item = items[itemIx];
-        if (itemIx === currentItem) {
-            item.classList.add("show");
-            item.classList.remove("hide");
-            console.log('show', item, reverse);
-        } else {
-            console.log('hide', item, reverse);
-            item.classList.add("hide");
-            item.classList.remove("show");
-        }
-    }
-}
+// Events listeners
