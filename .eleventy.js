39c9cd111e1cbf620bb05cb45e243e8b8a872270
Antoine <antoine.bernier@outlook.fr>
2020-09-29 16:28:14 +0200

    [*]: Edit to have a 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const svgContents = require('eleventy-plugin-svg-contents');
+const fs = require("fs");
 
 module.exports = function(eleventyConfig) {
     // Copy the static assets to the output folder
@@ -6,6 +7,23 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('img');
     eleventyConfig.addPlugin(svgContents);
+
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+          ready: function(err, bs) {
+    
+            bs.addMiddleware("*", (req, res) => {
+              const content_404 = fs.readFileSync('_site/404/index.html');
+              // Provides the 404 content without redirect.
+              res.write(content_404);
+              // Add 404 http status code in request header.
+              // res.writeHead(404, { "Content-Type": "text/html" });
+              res.writeHead(404);
+              res.end();
+            });
+          }
+        }
+      });
 
     // You can return your Config object (optional).
     return {
