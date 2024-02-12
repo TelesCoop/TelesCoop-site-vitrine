7284f3688b9fa35de35497c6af79d0ce27032fcd
Quentin Madura <quentin.madura@gmail.com>
2024-02-12 12:14:29 +0100

    feat: update categories filters behavior

--- js/telescoop.js
+++ js/telescoop.js
@@ -34,25 +34,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 });
-// Function to get active categories
-const getActiveCategories = () => {
-    const activeCategoryElements = document.querySelectorAll(".category-active");
-    return [...activeCategoryElements].map(el => el.getAttribute("category"));
-};
 
-// Function to filter projects list based on active categories
-const filterProjectsList = (activeCategories) => {
-    document.querySelectorAll('.project-card').forEach(projectCard => {
-        const projectCategories = projectCard.getAttribute("card-categories");
-        const isActive = activeCategories.some(category => projectCategories.includes(category));
-        projectCard.classList.toggle("is-hidden", !isActive);
-    });
-};
 
-// Function to toggle active class on an element
-const toggleActiveClass = (element, className) => {
-    element.classList.toggle(className);
-};
 
 // Function to apply styles based on active categories
 const applyStyle = (activeCategories) => {
@@ -67,11 +50,52 @@ const applyStyle = (activeCategories) => {
 };
 
 // Function to filter projects by category
+// Function to toggle active class on an element
+const toggleActiveClass = (element, className) => {
+    element.classList.toggle(className);
+};
+let firstSelection = true;
+
+// Function to get active categories
+const getActiveCategories = () => {
+    const activeCategoryElements = document.querySelectorAll(".category-active");
+    return [...activeCategoryElements].map(el => el.getAttribute("category"));
+};
+
+// Function to filter projects list based on active categories
+const filterProjectsList = (activeCategories) => {
+    document.querySelectorAll('.project-card').forEach(projectCard => {
+        const projectCategories = projectCard.getAttribute("card-categories");
+        const isActive = activeCategories.some(category => projectCategories.includes(category));
+        if (isActive) {
+            projectCard.classList.remove("is-hidden"); // show project if it matches an active category
+        } else {
+            projectCard.classList.add("is-hidden"); // hide project if it doesn't match any active category
+        }
+    });
+};
+
+// Function to filter projects by category
 const filterProjectsByCategory = (element) => {
-    const activeCategory = "category-active";
-    toggleActiveClass(element, activeCategory);
+    if (firstSelection) {
+        // remove active class from all elements
+        document.querySelectorAll('.category-active').forEach(el => el.classList.remove("category-active"));
+        firstSelection = false;
+    }
+    toggleActiveClass(element, "category-active");
 
     const activeCategories = getActiveCategories();
     filterProjectsList(activeCategories);
     applyStyle(activeCategories);
 };
+
+const resetProjectCategories = () => {
+    document.querySelectorAll('.category-active').forEach(el => el.classList.remove("category-active"));
+    document.querySelectorAll('.project-card').forEach(projectCard => projectCard.classList.remove("is-hidden"));
+    document.querySelectorAll('.filter-category').forEach(filterCategory => {
+        const [background, button] = [...filterCategory.children].filter(el => el.classList.contains("background") || el.classList.contains("button"));
+        background.classList.remove("is-hidden");
+        button.classList.remove("has-text-grey-dark");
+    });
+    firstSelection = true;
+}
